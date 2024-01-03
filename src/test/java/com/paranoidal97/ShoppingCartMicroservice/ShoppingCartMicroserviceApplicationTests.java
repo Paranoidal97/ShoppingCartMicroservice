985fc3a4b067dfb999792c43179b93e836043b72
paranoidal97 <LukaszRams1997@gmail.com>
2024-01-03 12:25:50 +0100

    initial commit

---
+++ src/test/java/com/paranoidal97/ShoppingCartMicroservice/ShoppingCartMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paranoidal97.ShoppingCartMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
